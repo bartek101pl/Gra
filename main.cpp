3fb126d0740351cdc5677006340a64de4454a6b2
Bartek <snieg45@gmail.com>
2021-10-28 11:11:37 +0200

    Finished first milestone V 0.0.1

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include "Engine/base/engineBase.hpp"
-#include "Game_old/MainScene.hpp"
+
 
 int main() {
     // simple implement
-    engine::base::SceneController::getInstance()->addScene(new MainScene("main"));
+
     engine::base::engineBase engine{};
     engine.init({false, true,800,600,60});
     engine.start(0);
